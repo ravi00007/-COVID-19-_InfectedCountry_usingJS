449e2e6b364606ea7a3c23bf3d4d46bb994cc7bc
RAVI KUMAR NONIA <33859950+ravi00007@users.noreply.github.com>
2020-04-04 22:38:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+function updateMap() {
+    console.log("Updating map with realtime data")
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(rsp => {
+            // console.log(rsp.data)
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+                
+               
+                Mark on the map
+                new mapboxgl.Marker({
+                    draggable: false,
+                     color: "rgb(255,0,0)"
+                }).setLngLat([longitude, latitude])
+                .addTo(map); 
+            });
+        })
+}
+
+updateMap();
